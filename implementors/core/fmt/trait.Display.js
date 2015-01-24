6d5a182f022bc1badd15159a50847a72dc2cc47a
Steven Allen <steven@stebalien.com>
2015-01-23 23:54:03 -0500

    Update docs for 233f0aaf3b42ef265bf24801002cd98ab2ae6e39
    Return actual errors.
    
    This is more versatile and makes things more debugable
    (`result.unwrap()` will print a nice error message).

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['coatcheck'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='coatcheck/struct.ClaimError.html' title='coatcheck::ClaimError'>ClaimError</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='coatcheck/struct.AccessError.html' title='coatcheck::AccessError'>AccessError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['coatcheck'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='coatcheck/enum.ErrorKind.html' title='coatcheck::ErrorKind'>ErrorKind</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='coatcheck/struct.AccessError.html' title='coatcheck::AccessError'>AccessError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
