9e2d61e353c40c1223646d68e70a0c09112c81e0
Steven Allen <steven@stebalien.com>
2015-02-09 16:27:02 -0500

    Update docs for 514db479cbf0e50781f361993853e3578b24b5ca
    Remove unecessary generic parameters.

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['coatcheck'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, I, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='coatcheck/struct.Tickets.html' title='coatcheck::Tickets'>Tickets</a>&lt;'a, I, V&gt;",];
+implementors['coatcheck'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, I&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='coatcheck/struct.Tickets.html' title='coatcheck::Tickets'>Tickets</a>&lt;'a, I&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
