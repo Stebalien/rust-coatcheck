7235be318f1f359208b5b97e3f6cd0dede9387bd
Steven Allen <steven@stebalien.com>
2015-04-03 14:28:24 -0400

    Update docs for 6bd77cea1923867c4664bb35c807a0a2083e1b88
    Update to the latest nightly.

--- implementors/core/ops/trait.IndexMut.js
+++ implementors/core/ops/trait.IndexMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['coatcheck'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a class='struct' href='coatcheck/struct.Ticket.html' title='coatcheck::Ticket'>Ticket</a>&gt; for <a class='struct' href='coatcheck/struct.CoatCheck.html' title='coatcheck::CoatCheck'>CoatCheck</a>&lt;V&gt;",];
+implementors['coatcheck'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;&amp;'a <a class='struct' href='coatcheck/struct.Ticket.html' title='coatcheck::Ticket'>Ticket</a>&gt; for <a class='struct' href='coatcheck/struct.CoatCheck.html' title='coatcheck::CoatCheck'>CoatCheck</a>&lt;V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
